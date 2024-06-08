f34eb6946604f4660f9bb20fe46836a9b87ff2a4
mennazm <91394925+mennazm@users.noreply.github.com>
2024-06-08 14:17:32 +0300

    medical rip mangment

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,16 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './admin/login/login.component';
-import { RegisterComponent } from './admin/register/register.component';
-import { NotfoundComponent } from './notfound/notfound.component';
+import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: 'admin/login', component: LoginComponent },
-  { path: 'admin/register', component: RegisterComponent },
-  { path: '', redirectTo: 'admin/login', pathMatch: 'full' },
-  { path: '**', component: NotfoundComponent }];
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
+export const routes: Routes = [];
+
--- Client/src/app/shared/navbar/navbar.component.spec.ts
+++ Client/src/app/shared/navbar/navbar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AdminComponent } from './admin.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('AdminComponent', () => {
-  let component: AdminComponent;
-  let fixture: ComponentFixture<AdminComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AdminComponent]
+      imports: [NavbarComponent]
     })
     .compileComponents();
-
-    fixture = TestBed.createComponent(AdminComponent);
+    
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
