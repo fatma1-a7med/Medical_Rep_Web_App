fc2aca604523a8781cd5ef5d4f6171005ca864a0
mennazm <91394925+mennazm@users.noreply.github.com>
2024-06-15 15:27:14 +0300

    activitymonitor

--- Frontend/src/polyfills.js
+++
@@ -1,9 +0,0 @@
-// polyfills.js
-const { JSDOM } = require('jsdom');
-
-// Create a mock DOM environment
-const dom = new JSDOM('<!doctype html><html><body></body></html>');
-
-// Get the mock window and document objects
-global.window = dom.window;
-global.document = dom.window.document;
